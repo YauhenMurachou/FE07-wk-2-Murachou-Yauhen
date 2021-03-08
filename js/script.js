102cc1ce220f4bc25629bdc1e1cfc615f053ca90
YauhenMurachou <emurach1992@gmail.com>
2021-03-08 23:47:41 +0300

    First

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document) .ready (function(){
+	$('.slider__wrapper') .slick({
+		arrows: false,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 1,
+		speed: 1000,
+		infinite: false, 
+		appendDots: $('.slider__dots')
+			});
+			// $('.slider__wrapper').on ('beforechange', function (event, slick, currentSlide, nextSlide){
+			// 	console.log(nextSlide);
+			// });
+})
